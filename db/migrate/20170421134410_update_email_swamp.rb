41ea5c824369a0ee1c25182b6067dbfe58736d99
andy <andy@ulendo.com>
2017-04-21 15:48:27 +0200

    Update

--- db/migrate/20170421134410_update_email_swamp.rb
+++ db/migrate/20170421134410_update_email_swamp.rb
@@ -1,7 +1,7 @@
 class UpdateEmailSwamp < ActiveRecord::Migration[5.0]
   def up
     t=Team.find_by_ref('swampdonkey')
-    t.email='brynn@rma.co.bw'
+    t.email='brynn.morgan@icloud.com'
     t.save!
   end
 end
